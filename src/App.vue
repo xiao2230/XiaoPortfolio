e5e8b67fd549a753e65124e3f61b1e6b7eee1a93
xiao2230 <xiaobow2230@gmail.com>
2023-09-13 01:01:53 +0800

    scss基礎設定(完成)

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,20 @@ import { RouterView } from 'vue-router';
 import Sidebar from '@/components/Sidebar.vue';
 import { ref } from 'vue';
 
-const theme = ref(null);
+const theme = ref("auto");
 </script>
 
 <template>
   <div class="app" :data-theme="theme">
     <Sidebar />
-
     <RouterView />
   </div>
 </template>
 
 <style lang="scss">
-@import '@/assets/scss/main.scss';
-
-.app{
-    background-color: $primaryColor;
+.app {
+  color: $secondaryColor;
+  background-color: $primaryColor;
+  display: flex;
 }
 </style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,8 +3,29 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/github">GitHub</RouterLink>
+    <aside>
+        <div class="asideTop">
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/github">GitHub</RouterLink>
+        </div>
+        <div class="asideBottom">
+            <button type="button">Change Theme</button>
+        </div>
+    </aside>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+aside {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    min-height: 100vh;
+    padding: 1rem;
+    border-right: 2px solid $secondaryColor;
+
+    .asideTop {
+        display: flex;
+        flex-direction: column;
+    }
+}
+</style>
