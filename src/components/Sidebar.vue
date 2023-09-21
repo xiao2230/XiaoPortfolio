d3fc139a0b5705d3a3b7b7a7ef8d21bd406017d7
xiao2230 <xiaobow2230@gmail.com>
2023-09-21 14:08:55 +0800

    Update Sidebar.vue

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -207,7 +207,7 @@ aside {
     nav {
         display: flex;
         flex-direction: column;
-        justify-content: center;
+        justify-content: space-between;
         flex-grow: 1;
         padding-block: 1rem;
 
