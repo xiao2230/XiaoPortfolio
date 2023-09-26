56cb61011b2cb066e3429eab96e89ec2f7c8980b
xiao2230 <xiaobow2230@gmail.com>
2023-09-26 16:29:47 +0800

    找出netlify gsap無用的問題

--- src/stores/load.js
+++ src/stores/load.js
@@ -3,25 +3,21 @@ import { defineStore } from "pinia";
 
 export const useLoadStore = defineStore("load", () => {
     const load = ref("loading");
-    const imgsUrl = ["src/assets/img/bg.png", "src/assets/img/land_1.png", "src/assets/img/land_2.png", "src/assets/img/planet_1.png", "src/assets/img/planet_2.png", "src/assets/img/planet_3.png"];
+    const imgsUrl = ["/src/assets/img/bg.png", "/src/assets/img/land_1.png", "/src/assets/img/land_2.png", "/src/assets/img/planet_1.png", "/src/assets/img/planet_2.png", "/src/assets/img/planet_3.png"];
     const chkLoad = imgsUrl => {
         let i = 0;
         imgsUrl.forEach(imgUrl => {
-            console.log(imgUrl);
             const img = new Image();
             img.src = imgUrl;
-            img.onload = () => {
-                console.log("load：", imgUrl);
+            img.addEventListener("load", () => {
                 i += 1;
                 if (i === imgsUrl.length) {
                     load.value = "loaded";
-                    console.log("loaded");
                 }
-            };
+            });
         })
     };
     onMounted(() => {
-        console.log("onMounted");
         chkLoad(imgsUrl);
     });
 
