fb38415c56a71f02655301d94ea4773b65ec0ba7
xiao2230 <xiaobow2230@gmail.com>
2023-09-26 16:27:35 +0800

    找出netlify gsap無用的問題

--- src/stores/load.js
+++ src/stores/load.js
@@ -3,7 +3,7 @@ import { defineStore } from "pinia";
 
 export const useLoadStore = defineStore("load", () => {
     const load = ref("loading");
-    const imgsUrl = ["/src/assets/img/bg.png", "/src/assets/img/land_1.png", "/src/assets/img/land_2.png", "/src/assets/img/planet_1.png", "/src/assets/img/planet_2.png", "/src/assets/img/planet_3.png"];
+    const imgsUrl = ["src/assets/img/bg.png", "src/assets/img/land_1.png", "src/assets/img/land_2.png", "src/assets/img/planet_1.png", "src/assets/img/planet_2.png", "src/assets/img/planet_3.png"];
     const chkLoad = imgsUrl => {
         let i = 0;
         imgsUrl.forEach(imgUrl => {
