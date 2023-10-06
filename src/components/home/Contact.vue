d911828c679d94ba30e449366199103529513dfd
xiao2230 <xiaobow2230@gmail.com>
2023-10-06 15:43:08 +0800

    Contact製作(完成)

--- src/components/home/Contact.vue
+++ src/components/home/Contact.vue
@@ -6,6 +6,7 @@ const guestEmail = ref("");
 const guestMessage = ref("");
 const isDisabled = ref(false);
 const sendBtnText = ref("SEND");
+const toast = ref("hide");
 
 const send = () => {
     isDisabled.value = true;
@@ -16,8 +17,12 @@ const send = () => {
         guestMessage.value = "";
         isDisabled.value = false;
         sendBtnText.value = "SEND";
+        toast.value = "show";
+        setTimeout(() => toast.value = "hide", 5000);
     }, 2000);
 }
+
+const closeToast = () => toast.value = "hide";
 
 onMounted(() => {
     if (sessionStorage["guestMessage"]) guestMessage.value = sessionStorage["guestMessage"];
@@ -50,17 +55,20 @@ onMounted(() => {
                 </div>
             </form>
         </div>
-        <div class="toast" role="alert">
+    </section>
+
+    <Transition name="toast">
+        <div v-show="toast === 'show'" class="toast" role="alert">
             <div class="toastHeader">
                 <span class="logo">XP</span>
                 <strong class="name">XiaoPortfolio</strong>
-                <button type="button" class="close"><span></span></button>
+                <button type="button" class="close" @click="closeToast"><span></span></button>
             </div>
             <div class="toastBody">
-                
+                Sent successfully
             </div>
         </div>
-    </section>
+    </Transition>
 </template>
 
 <style lang="scss" scoped>
@@ -120,6 +128,82 @@ onMounted(() => {
                 transform: translateY(-1rem);
             }
         }
+    }
+}
+
+.toast {
+    color: $primaryColor;
+    width: 300px;
+    margin: 1rem;
+    border: 1px solid $primaryColor;
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    z-index: 999;
+
+    .toastHeader {
+        padding: 0.5rem 0.8rem;
+        background-color: $secondaryColor;
+
+        .logo {
+            color: $secondaryColor;
+            background-color: $primaryColor;
+            width: 2rem;
+            aspect-ratio: 1;
+            border-radius: 50%;
+            display: inline-flex;
+            justify-content: center;
+            align-items: center;
+            margin-right: 0.5rem;
+        }
+
+        .close {
+            width: 1.5rem;
+            aspect-ratio: 1;
+            float: right;
+            cursor: pointer;
+
+            &:hover {
+                filter: drop-shadow(0 0 2px $primaryColor) drop-shadow(0 0 8px $primaryColor);
+            }
+
+            span {
+                display: inline-block;
+                height: 4px;
+                width: 100%;
+                background-color: $primaryColor;
+                transform: rotateZ(45deg);
+                position: relative;
+
+                &::after {
+                    content: "";
+                    display: inline-block;
+                    height: 4px;
+                    width: 100%;
+                    background-color: $primaryColor;
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    transform: rotateZ(90deg);
+                }
+            }
+        }
+    }
+
+    .toastBody {
+        padding: 0.8rem;
+        background-color: $fourthColor;
+    }
+
+    &.toast-enter-active,
+    &.toast-leave-active {
+        transition: opacity 0.5s ease, transform 0.8s ease;
+    }
+
+    &.toast-enter-from,
+    &.toast-leave-to {
+        opacity: 0;
+        transform: translateY(2rem);
     }
 }
 </style>
