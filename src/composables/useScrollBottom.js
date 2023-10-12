8dbe6ceeb017e531a5b6cf5e3f3a77e925a26135
xiao2230 <xiaobow2230@gmail.com>
2023-10-12 23:17:04 +0800

    測試BUG

--- src/composables/useScrollBottom.js
+++ src/composables/useScrollBottom.js
@@ -4,13 +4,13 @@ import { debounce } from "@/lib/Debounce.js";
 export default function useScrollBottom() {
     const isBottom = ref(false);
     const isScroll = ref(false);
-    const triggerEarly = 100;
+    const triggerEarly = 120;
 
     const chkScrollBottom = debounce(() => {
         isScroll.value = !isScroll.value
         const { clientHeight, scrollTop, scrollHeight } = document.documentElement;
         clientHeight + scrollTop >= scrollHeight - triggerEarly ? isBottom.value = true : isBottom.value = false;
-    });
+    }, 300);
 
     onMounted(() => window.addEventListener("scroll", chkScrollBottom));
     onUnmounted(() => window.removeEventListener("scroll", chkScrollBottom));
