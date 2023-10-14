2ee36b5c53bcff0d96937b31d7817dc6d7512bc0
xiao2230 <xiaobow2230@gmail.com>
2023-10-14 08:50:42 +0800

    測試BUG：切換router頁面掛掉

--- src/router/index.js
+++ src/router/index.js
@@ -29,7 +29,10 @@ const router = createRouter({
   scrollBehavior(to, from, savedPosition) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve({ left: 0, top: 0 })
+        resolve({
+          el: 'body',
+          top: 0
+        })
       }, 300)
     })
   },
