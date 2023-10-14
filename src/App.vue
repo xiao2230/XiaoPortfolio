2e3b2835b3759b3a8629f43d956e15fc6eca157b
xiao2230 <xiaobow2230@gmail.com>
2023-10-14 23:26:07 +0800

    測試scroll卡頓

--- src/App.vue
+++ src/App.vue
@@ -31,12 +31,11 @@ onMounted(() => {
   <div ref="app" :data-load="load" :data-theme="theme" class="app">
     <Loading />
     <Sidebar />
-    <RouterView />
-    <!-- <RouterView v-slot="{ Component }">
+    <RouterView v-slot="{ Component }">
       <Transition name="router-fade" mode="out-in">
         <component :is="Component" />
       </Transition>
-    </RouterView> -->
+    </RouterView>
   </div>
 </template>
 
@@ -48,13 +47,13 @@ onMounted(() => {
   overflow-y: hidden;
 }
 
-// .router-fade-enter-active,
-// .router-fade-leave-active {
-//   transition: opacity 0.3s ease-in-out;
-// }
+.router-fade-enter-active,
+.router-fade-leave-active {
+  transition: opacity 0.3s ease-in-out;
+}
 
-// .router-fade-enter-from,
-// .router-fade-leave-to {
-//   opacity: 0;
-// }
+.router-fade-enter-from,
+.router-fade-leave-to {
+  opacity: 0;
+}
 </style>
